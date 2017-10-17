9549623f5a4f6104387688318b73777ec99cb5ce
bmizhen <bmizhen@users.noreply.github.com>
2017-10-16 20:25:50 -0400

    Update project.clj

--- genetic-alg/project.clj
+++ genetic-alg/project.clj
@@ -1,5 +1,5 @@
 (defproject genetic "0.0.1-SNAPSHOT"
-  :description "Example of a genetic algorythm in clojure."
+  :description "Example of a genetic algorithm in clojure."
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :source-paths ["."]
   :main genetic)
